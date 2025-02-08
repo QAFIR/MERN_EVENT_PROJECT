2c8c06f7ffa533d82e3647961b12e79eb6dd5d5d
abhishekashu05 <abhishekashu05@gmail.com>
2025-02-08 10:08:50 +0530

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -19,6 +19,12 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ 
+    success: true,
+    message: "API is running",
+   });
+});
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
